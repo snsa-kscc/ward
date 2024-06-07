6effd9468d1648c1f942db5b1244fb227a73baef
snsa-kscc <sinisa@dvasadva.com>
2024-06-07 19:41:42 +0200

    edit db file

--- dist/server/chunks/index_BfuacQqL.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_uFH1YDEw.mjs');
-
-export { page };
--- dist/server/chunks/insert_DvbaO4Aa.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/insert_BVqqWIC8.mjs');
-
-export { page };
