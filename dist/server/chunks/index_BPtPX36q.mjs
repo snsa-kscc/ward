8372cb7430dcfc8df7c03d816888cd43c1b98356
snsa-kscc <sinisa@dvasadva.com>
2024-07-03 19:05:17 +0200

    del dist

--- dist/server/chunks/index_BPtPX36q.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_BmO3O0Bh.mjs');
-
-export { page };
--- dist/server/chunks/prerender_BqzyHbU9.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_CaTSi9w_.mjs');
-
-export { page };
