3d0c262d6004d3eccd8380681a6f92bdf894bb32
snsa-kscc <sinisa@dvasadva.com>
2024-07-05 14:07:13 +0200

    add localhost

--- db/index.ts
+++ db/index.ts
@@ -2,11 +2,21 @@ import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import * as schema from "./schema";
 
-const poolConnection = mysql.createPool({
-  host: import.meta.env.MYSQL_HOST,
-  user: import.meta.env.MYSQL_USER,
-  database: import.meta.env.MYSQL_DATABASE,
-  password: import.meta.env.MYSQL_PASSWORD,
-});
+let poolConnection;
+if (process.env.NODE_ENV !== "production") {
+  poolConnection = mysql.createPool({
+    host: import.meta.env.MYSQL_HOST,
+    user: import.meta.env.MYSQL_USER,
+    database: import.meta.env.MYSQL_DATABASE,
+    password: import.meta.env.MYSQL_PASSWORD,
+  });
+} else {
+  poolConnection = mysql.createPool({
+    host: "localhost",
+    user: import.meta.env.MYSQL_USER,
+    database: import.meta.env.MYSQL_DATABASE,
+    password: import.meta.env.MYSQL_PASSWORD,
+  });
+}
 
 export const myDb = drizzle(poolConnection, { schema, mode: "default" });
