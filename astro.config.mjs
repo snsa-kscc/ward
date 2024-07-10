e28fabfaf05f08dfaea6e3b3efcf36dde6bd1634
snsa-kscc <sinisa@dvasadva.com>
2024-07-10 19:48:31 +0200

    add shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from "astro/config";
 //import db from "@astrojs/db";
 import node from "@astrojs/node";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
   output: "server",
   adapter: node({
     mode: "middleware",
