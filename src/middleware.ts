1c4baf19b7e78079d2da4d35447f0c4693860226
snsa-kscc <sinisa@dvasadva.com>
2024-07-13 06:34:38 +0200

    add auth

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { defineMiddleware } from "astro:middleware";
+
+export const onRequest = defineMiddleware((context, next) => {
+  const url = new URL(context.request.url);
+
+  if (url.pathname.startsWith("/admin")) {
+    const basicAuth = context.request.headers.get("authorization");
+
+    if (basicAuth) {
+      const [user, password] = Buffer.from(basicAuth.split(" ")[1], "base64").toString().split(":");
+
+      if (user === import.meta.env.SITE_USER && password === import.meta.env.SITE_PASSWORD) {
+        return next();
+      }
+    }
+
+    return new Response("Auth required", {
+      status: 401,
+      headers: {
+        "WWW-authenticate": 'Basic realm="Secure Area"',
+      },
+    });
+  }
+
+  return next();
+});
