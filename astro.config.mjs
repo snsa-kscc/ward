06d209ef9e34fd35c282d62a6d5510220a845c35
snsa-kscc <sinisa@dvasadva.com>
2024-08-21 00:22:17 +0200

    edit index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     defaultLocale: "en",
     locales: ["es", "en", "fr"],
     routing: {
-      prefixDefaultLocale: false,
+      prefixDefaultLocale: true,
     },
   },
   integrations: [
