a3e065eb821a24b51a248fb0495550f1343f395b
snsa-kscc <sinisa@dvasadva.com>
2024-10-02 00:31:59 +0200

    edit footer data

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -6,3 +6,36 @@ declare global {
     ScrollTrigger: typeof ScrollTrigger;
   }
 }
+
+export type GridData = {
+  [key: string]: string | number | Date;
+  media: string;
+  title: string;
+  slug: string;
+}[][];
+
+export type StoreData = {
+  [key: string]: string;
+  siteTitleFirst: string;
+  siteTitleSecond: string;
+  siteDescriptionFirst: string;
+  siteDescriptionSecond: string;
+  aboutKey: string;
+  about: string;
+  specialitiesKey: string;
+  specialities: string;
+  specialitiesDescription: string;
+  projects: string;
+  ctaHook: string;
+  ctaButton: string;
+  ctaHeading: string;
+  contactKey: string;
+  contactEmail: string;
+  contactPhone: string;
+  contactAddress: string;
+  infoKey: string;
+  newsletterKey: string;
+  newsletterButton: string;
+  newsletterDisclaimer: string;
+  copyright: string;
+};
